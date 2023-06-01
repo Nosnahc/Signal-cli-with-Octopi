9d67c6e0ff4d517c952af7f075204b7b6b74f0b1
Nosnahc <123942344+Nosnahc@users.noreply.github.com>
2023-06-01 15:51:49 +0200

    Update comments

--- signal-cli-install.sh
+++ signal-cli-install.sh
@@ -36,16 +36,18 @@ fi
 
 mkdir /tmp/signal-cli-install
 
+#Download signal-cli
 curl --proto '=https' --tlsv1.2 -L -o /tmp/signal-cli-install/signal-cli-"${VERSION}"-Linux.tar.gz https://github.com/AsamK/signal-cli/releases/download/v"${VERSION}"/signal-cli-"${VERSION}"-Linux.tar.gz
 tar xf /tmp/signal-cli-install/signal-cli-"${VERSION}"-Linux.tar.gz -C /opt
 rm /tmp/signal-cli-install/signal-cli-"${VERSION}"-Linux.tar.gz
+#Catch the good libsignal version from the previous signal-cli downloaded
 export LIBVERSION=$(find /opt/signal-cli-"${VERSION}"/lib/ -maxdepth 1 -mindepth 1 -name 'libsignal-client-*' | sed -E 's/\/opt\/signal-cli-[0-9]{1,}.[0-9]{1,}.[0-9]{1,}\/lib\/libsignal-client-*//g' | sed -E 's/.jar//g')
 ln -sf /opt/signal-cli-"${VERSION}"/bin/signal-cli /usr/local/bin/
 
 # libsignal
-#For 64bits distribution
+#Download for 64bits distribution
 #curl --proto '=https' --tlsv1.2 -L -o /tmp/signal-cli-install/libsignal.tar.gz https://github.com/exquo/signal-libs-build/releases/download/libsignal_v"${LIBVERSION}"/libsignal_jni.so-v"${LIBVERSION}"-aarch64-unknown-linux-gnu.tar.gz
-#For octopi
+#Download for octopi
 curl --proto '=https' --tlsv1.2 -L -o /tmp/signal-cli-install/libsignal.tar.gz https://github.com/exquo/signal-libs-build/releases/download/libsignal_v"${LIBVERSION}"/libsignal_jni.so-v"${LIBVERSION}"-armv7-unknown-linux-gnueabihf.tar.gz
 tar xf /tmp/signal-cli-install/libsignal.tar.gz -C /tmp/signal-cli-install/
 rm /tmp/signal-cli-install/libsignal.tar.gz
@@ -54,7 +56,6 @@ rm /tmp/signal-cli-install/libsignal.tar.gz
 # replace libsignal_jni.so
 zip -d /opt/signal-cli-${VERSION}/lib/libsignal-client-*.jar libsignal_jni.so
 zip /opt/signal-cli-${VERSION}/lib/libsignal-client-*.jar /tmp/signal-cli-install/libsignal_jni.so
-#zip /opt/signal-cli-${VERSION}/lib/libsignal-client-*.jar /tmp/signal-cli-install/libsignal/target/release/libsignal_jni.so
 
 # fallback of libsignal_jni.so
 ## create folder if it dosent exist
@@ -65,7 +66,6 @@ fi
 
 ## copy libsignal_jni.so to Java library path
 cp  /tmp/signal-cli-install/libsignal_jni.so /usr/java/packages/lib
-#cp  /tmp/signal-cli-install/libsignal/target/release/libsignal_jni.so /usr/java/packages/lib
 
 # permissions
 chown root:root /usr/java/packages/lib/libsignal_jni.so
